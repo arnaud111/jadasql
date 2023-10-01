ab9d960ffea381fe3583f4ed94b41e5caf1919da
MacaronFR <denisft77@gmail.com>
2023-10-01 18:45:47 +0200

    Reformat

--- include/index/Node.hpp
+++ include/index/Node.hpp
@@ -15,8 +15,8 @@ class Node {
 public:
 	explicit Node(int m) {
 		max = m;
-		keys = (K**)malloc(sizeof(K*) * m);
-		children = (Node<V, K>**) malloc((sizeof(Node<V, K>*) * (m+1)));
+		keys = (K **) malloc(sizeof(K *) * m);
+		children = (Node<V, K> **) malloc((sizeof(Node<V, K> *) * (m + 1)));
 	}
 
 	K **keys;
@@ -34,45 +34,44 @@ public:
 	}
 
 	virtual void insertAt(K *key, V *value) {
-		for(int i = 0; i < n; ++i) {
-			if(*key < *(keys[i])) {
-				if(children[i]->canInsert(key)) {
+		for (int i = 0; i < n; ++i) {
+			if (*key < *(keys[i])) {
+				if (children[i]->canInsert(key)) {
 					children[i]->insertAt(key, value);
 					return;
 				} else {
 					insertKey(children[i]->keys[max / 2]);
 					auto tmp = children[i]->split();
-					insertChild(i+1, tmp);
+					insertChild(i + 1, tmp);
 					this->n++;
-					if(*key < *(keys[i])){
+					if (*key < *(keys[i])) {
 						children[i]->insertAt(key, value);
 					} else {
-						children[i+1] ->insertAt(key, value);
+						children[i + 1]->insertAt(key, value);
 					}
 					return;
 				}
 			}
 		}
-		if(children[n]->canInsert(key)) {
+		if (children[n]->canInsert(key)) {
 			children[n]->insertAt(key, value);
 		} else {
 			insertKey(children[n]->keys[max / 2]);
 			auto tmp = children[n]->split();
-			insertChild(n+1, tmp);
+			insertChild(n + 1, tmp);
 			this->n++;
-			if(*key < *(keys[n])){
+			if (*key < *(keys[n])) {
 				children[n]->insertAt(key, value);
 			} else {
-				children[n+1] ->insertAt(key, value);
+				children[n + 1]->insertAt(key, value);
 			}
 		}
-		return;
 	}
 
 	virtual bool canInsert(K *key) {
-		for(int i = 0; i < n; ++i) {
-			if(*key < *(keys[i])) {
-				if(children[i]->canInsert(key)) {
+		for (int i = 0; i < n; ++i) {
+			if (*key < *(keys[i])) {
+				if (children[i]->canInsert(key)) {
 					return true;
 				}
 				return n < max;
@@ -81,23 +80,23 @@ public:
 		return n < max;
 	}
 
-	virtual Node<V, K>* split() {
+	virtual Node<V, K> *split() {
 		auto *newNode = new Node<V, K>(max);
-		for(int i = (max+1)/2; i < max; ++i){
-			newNode->children[i - (max+1/2)] = children[i];
-			newNode->keys[i - (max+1)/2] = keys[i];
+		for (int i = (max + 1) / 2; i < max; ++i) {
+			newNode->children[i - (max + 1 / 2)] = children[i];
+			newNode->keys[i - (max + 1) / 2] = keys[i];
 			children[i] = nullptr;
 			keys[i] = nullptr;
 		}
 		newNode->n = max / 2;
 		this->n = max / 2;
-		newNode->children[max - (max+1)/2] = children[max];
+		newNode->children[max - (max + 1) / 2] = children[max];
 		children[max] = nullptr;
 		return newNode;
 	}
 
 	void insertChild(int index, Node *child) {
-		for(int i = n; i >= index; --i) {
+		for (int i = n; i >= index; --i) {
 			children[i + 1] = children[i];
 		}
 		children[index] = child;
@@ -111,8 +110,8 @@ public:
 	}
 
 	void insertKey(K *key) {
-		for(int i = 0; i < n; ++i) {
-			if(*key < *(keys[i])){
+		for (int i = 0; i < n; ++i) {
+			if (*key < *(keys[i])) {
 				insertKey(i, key);
 				return;
 			}
@@ -129,9 +128,9 @@ class LeafNode : public Node<V, K> {
 public:
 	explicit LeafNode(int m) : Node<V, K>(m) {
 		this->max = m;
-		this->keys = (K**)malloc(sizeof(K*) * m);
+		this->keys = (K **) malloc(sizeof(K *) * m);
 		this->children = nullptr;
-		this->records = (V**) malloc(sizeof(V*) * (m+1));
+		this->records = (V **) malloc(sizeof(V *) * (m + 1));
 	};
 
 	V **records;
@@ -140,7 +139,7 @@ public:
 
 	V *getValueAt(K *key) {
 		for (int i = 0; i < this->n; ++i) {
-			if(*(this->keys[i]) == *key) {
+			if (*(this->keys[i]) == *key) {
 				return records[i];
 			}
 		}
@@ -148,8 +147,8 @@ public:
 	}
 
 	void insertAt(K *key, V *value) {
-		for(int i = 0; i < this->n; ++i) {
-			if(*key < *(this->keys[i])) {
+		for (int i = 0; i < this->n; ++i) {
+			if (*key < *(this->keys[i])) {
 				this->insertKey(i, key);
 				this->insertRecord(i, value);
 				this->n++;
@@ -161,17 +160,17 @@ public:
 		this->n++;
 	}
 
-	Node<V, K>* split() {
+	Node<V, K> *split() {
 		auto *newNode = new LeafNode<V, K>(this->max);
-		for(int i = (this->max+1)/2; i < this->max; ++i){
-			newNode->records[i - (this->max+1)/2] = this->records[i];
-			newNode->keys[i - (this->max+1)/2] = this->keys[i];
+		for (int i = (this->max + 1) / 2; i < this->max; ++i) {
+			newNode->records[i - (this->max + 1) / 2] = this->records[i];
+			newNode->keys[i - (this->max + 1) / 2] = this->keys[i];
 			this->records[i] = nullptr;
 			this->keys[i] = nullptr;
 		}
 		newNode->n = this->max / 2;
-		this->n = (this->max+1) / 2;
-		newNode->records[this->max - (this->max+1)/2] = this->records[this->max];
+		this->n = (this->max + 1) / 2;
+		newNode->records[this->max - (this->max + 1) / 2] = this->records[this->max];
 		this->records[this->max] = nullptr;
 		return newNode;
 	}
@@ -183,7 +182,7 @@ private:
 	}
 
 	void insertRecord(int index, V *value) {
-		for(int i = this->n; i >= index; --i) {
+		for (int i = this->n; i >= index; --i) {
 			records[i + 1] = records[i];
 		}
 		records[index] = value;

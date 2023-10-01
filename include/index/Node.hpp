2c8b3ab0b956878dbbf643367502cca141f18a7f
MacaronFR <denisft77@gmail.com>
2023-10-01 18:21:50 +0200

    Insert data in tree

--- include/index/Node.hpp
+++ include/index/Node.hpp
@@ -4,42 +4,188 @@
 #include <utils/Comparable.hpp>
 #include <type_traits>
 #include <cstdio>
+#include <cstdlib>
+#include <utils/instanceof.hpp>
 
-/*template <typename T>
-concept ComparableContraint = requires(T a) {
-	{ std::is_base_of<Comparable<T>, T>::value };
-};*/
+template<typename V, comparable K>
+class LeafNode;
 
-template<typename V, typename K>
+template<typename V, comparable K>
 class Node {
 public:
-	Comparable<K> **keys = nullptr;
+	explicit Node(int m) {
+		max = m;
+		keys = (K**)malloc(sizeof(K*) * m);
+		children = (Node<V, K>**) malloc((sizeof(Node<V, K>*) * (m+1)));
+	}
+
+	K **keys;
 	int n = 0;
-	Node<V, K> **children = nullptr;
+	Node<V, K> **children;
+	int max;
 
 	virtual V *getValueAt(K *key) {
 		for (int i = 0; i < n; ++i) {
-			if (*key <= *(keys[i])) {
+			if (*key < *(keys[i])) {
 				return children[i]->getValueAt(key);
 			}
 		}
 		return children[n]->getValueAt(key);
 	}
+
+	virtual void insertAt(K *key, V *value) {
+		for(int i = 0; i < n; ++i) {
+			if(*key < *(keys[i])) {
+				if(children[i]->canInsert(key)) {
+					children[i]->insertAt(key, value);
+					return;
+				} else {
+					insertKey(children[i]->keys[max / 2]);
+					auto tmp = children[i]->split();
+					insertChild(i+1, tmp);
+					this->n++;
+					if(*key < *(keys[i])){
+						children[i]->insertAt(key, value);
+					} else {
+						children[i+1] ->insertAt(key, value);
+					}
+					return;
+				}
+			}
+		}
+		if(children[n]->canInsert(key)) {
+			children[n]->insertAt(key, value);
+		} else {
+			insertKey(children[n]->keys[max / 2]);
+			auto tmp = children[n]->split();
+			insertChild(n+1, tmp);
+			this->n++;
+			if(*key < *(keys[n])){
+				children[n]->insertAt(key, value);
+			} else {
+				children[n+1] ->insertAt(key, value);
+			}
+		}
+		return;
+	}
+
+protected:
+
+	virtual Node<V, K>* split() {
+		auto *newNode = new Node<V, K>(max);
+		for(int i = (max+1)/2; i < max; ++i){
+			newNode->children[i - (max+1/2)] = children[i];
+			newNode->keys[i - (max+1)/2] = keys[i];
+			children[i] = nullptr;
+			keys[i] = nullptr;
+		}
+		newNode->n = max / 2;
+		this->n = max / 2;
+		newNode->children[max - (max+1)/2] = children[max];
+		children[max] = nullptr;
+		return newNode;
+	}
+
+	virtual bool canInsert(K *key) {
+		for(int i = 0; i < n; ++i) {
+			if(*key < *(keys[i])) {
+				if(children[i]->canInsert(key)) {
+					return true;
+				}
+				return n < max;
+			}
+		}
+	}
+
+	void insertKey(int index, K *key) {
+		for (int i = n - 1; i >= index; --i) {
+			keys[i + 1] = keys[i];
+		}
+		keys[index] = key;
+	}
+
+	void insertChild(int index, Node *child) {
+		for(int i = n; i >= index; --i) {
+			children[i + 1] = children[i];
+		}
+		children[index] = child;
+	}
+
+	void insertKey(K *key) {
+		for(int i = 0; i < n; ++i) {
+			if(*key < *(keys[i])){
+				insertKey(i, key);
+				return;
+			}
+		}
+		insertKey(n, key);
+	}
+
 };
 
-template<typename V, typename K>
+template<typename V, comparable K>
 class LeafNode : public Node<V, K> {
 public:
+	explicit LeafNode(int m) : Node<V, K>(m) {
+		this->max = m;
+		this->keys = (K**)malloc(sizeof(K*) * m);
+		this->children = nullptr;
+		this->records = (V**) malloc(sizeof(V*) * (m+1));
+	};
+
 	V **records = nullptr;
+
+	LeafNode *next = nullptr;
 
 	V *getValueAt(K *key) {
 		for (int i = 0; i < this->n; ++i) {
-			printf("%p\n", this->keys);
 			if(*(this->keys[i]) == *key) {
 				return records[i];
 			}
 		}
 		return (V *) nullptr;
+	}
+
+	void insertAt(K *key, V *value) {
+		for(int i = 0; i < this->n; ++i) {
+			if(*key < *(this->keys[i])) {
+				this->insertKey(i, key);
+				this->insertRecord(i, value);
+				this->n++;
+				return;
+			}
+		}
+		this->insertKey(this->n, key);
+		this->insertRecord(this->n, value);
+		this->n++;
+	}
+
+private:
+
+	bool canInsert(K *key) {
+		return this->n < this->max;
+	}
+
+	void insertRecord(int index, V *value) {
+		for(int i = this->n; i >= index; --i) {
+			records[i + 1] = records[i];
+		}
+		records[index] = value;
+	}
+
+	Node<V, K>* split() {
+		auto *newNode = new LeafNode<V, K>(this->max);
+		for(int i = (this->max+1)/2; i < this->max; ++i){
+			newNode->records[i - (this->max+1)/2] = this->records[i];
+			newNode->keys[i - (this->max+1)/2] = this->keys[i];
+			this->records[i] = nullptr;
+			this->keys[i] = nullptr;
+		}
+		newNode->n = this->max / 2;
+		this->n = this->max / 2;
+		newNode->records[this->max - (this->max+1)/2] = this->records[this->max];
+		this->records[this->max] = nullptr;
+		return newNode;
 	}
 };
 
--- main.cpp
+++ main.cpp
@@ -36,53 +36,56 @@ public:
 	}
 };
 
-template <typename T>
-class Test {
-public:
-	virtual bool operator==(T const& other) const = 0;
-};
-
-class MyInt2: Test<MyInt2> {
-private:
-	int value;
-
-public:
-	MyInt2(int v) {
-		value = v;
-	}
-
-	bool operator==(MyInt2 const& other) const override{
-		return value == other.value;
-	}
-};
-
 int main() {
-	MyInt2 **t = (MyInt2**)malloc(sizeof(MyInt2*) * 2);
-	t[0] = new MyInt2(2);
-	t[1] = new MyInt2(3);
-	bool a = t[0] == t[1];
-	printf("%d\n", a);
 
-	auto *test = new Node<MyInt, MyInt>();
-	MyInt(5) == MyInt(7);
+	auto *test = new Node<MyInt, MyInt>(5);
 	test->keys = (MyInt**) malloc(sizeof(MyInt*) * 2);
-	test->keys[0] = new MyInt(8);
-	test->keys[1] = new MyInt(12);
-	test->children = (Node<MyInt, MyInt>**)malloc(sizeof(Node<MyInt, MyInt>*) * 3);
+	test->keys[0] = new MyInt(10);
+	test->keys[1] = new MyInt(20);
+	test->children = (Node<MyInt, MyInt>**)malloc(sizeof(Node<MyInt, MyInt>*) * 2);
 	test->n = 2;
-	test->children[0] = new LeafNode<MyInt, MyInt>();
-	test->children[1] = new LeafNode<MyInt, MyInt>();
-	test->children[2] = new LeafNode<MyInt, MyInt>();
+	test->children[0] = new LeafNode<MyInt, MyInt>(5);
+	test->children[1] = new LeafNode<MyInt, MyInt>(5);
 	test->children[0]->n = 2;
 	test->children[0]->keys = (MyInt**) malloc(sizeof(MyInt*) * 2);
 	test->children[0]->keys[0] = new MyInt(2);
 	test->children[0]->keys[1] = new MyInt(6);
-	((LeafNode<MyInt, MyInt>*)test->children[0])->records = (MyInt**)malloc(sizeof(MyInt*) * 2);
+	test->children[1]->n = 0;
+	test->children[1]->keys = (MyInt**) malloc(sizeof(MyInt*) * 0);
 	((LeafNode<MyInt, MyInt>*)test->children[0])->records[0] = new MyInt(2);
 	((LeafNode<MyInt, MyInt>*)test->children[0])->records[1] = new MyInt(6);
-	auto tmp1 = MyInt(2);
-	auto *tmp = test->getValueAt(&tmp1);
-	printf("MyInt = %d", tmp->value);
+
+	printf("test->n = %d\n", test->n);
+	for(int i = 0; i < test->n; ++i) {
+		printf("test->children[%d]->n = %d\n", i, test->children[i]->n);
+		for(int j = 0; j < test->children[i]->n; ++j) {
+			printf("test->children[%d]->records[%d] = %d\n", i, j,  ((LeafNode<MyInt, MyInt>*)test->children[i])->records[j]->value);
+		}
+		printf("test->keys[%d] = %d\n", i, test->keys[i]->value);
+	}
+
+	test->insertAt(new MyInt(4), new MyInt(4));
+	test->insertAt(new MyInt(5), new MyInt(5));
+	test->insertAt(new MyInt(3), new MyInt(3));
+
+	printf("\ntest->n = %d\n", test->n);
+	for(int i = 0; i < test->n; ++i) {
+		printf("test->children[%d]->n = %d\n", i, test->children[i]->n);
+		for(int j = 0; j < test->children[i]->n; ++j) {
+			printf("test->children[%d]->records[%d] = %d\n", i, j,  ((LeafNode<MyInt, MyInt>*)test->children[i])->records[j]->value);
+		}
+		printf("test->keys[%d] = %d\n", i, test->keys[i]->value);
+	}
+
+	test->insertAt(new MyInt(8), new MyInt(8));
+	printf("\ntest->n = %d\n", test->n);
+	for(int i = 0; i < test->n; ++i) {
+		printf("test->children[%d]->n = %d\n", i, test->children[i]->n);
+		for(int j = 0; j < test->children[i]->n; ++j) {
+			printf("test->children[%d]->records[%d] = %d\n", i, j,  ((LeafNode<MyInt, MyInt>*)test->children[i])->records[j]->value);
+		}
+		printf("test->keys[%d] = %d\n", i, test->keys[i]->value);
+	}
 
 	return 1;
 }

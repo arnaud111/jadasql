10c8e9ac93d86a039af744d027c599ee0d77dc5a
nono <arnaudjourdain111@gmail.com>
2023-12-11 21:10:01 +0100

    request from command line

--- main.cpp
+++ main.cpp
@@ -1,51 +1,25 @@
+#include <iostream>
 #include "include/lexer/Lexer.h"
 #include "include/parser/Parser.h"
 
 int main() {
 
-    string request = "SELECT DISTINCT T.col, _Uwu1, \"UwU\\\"'\", 3, MAX(CONCAT(\"test\", c.coucou)) FROM test@t_test T LEFT JOIN ta ON ta.id = T.id INNER JOIN tt ON 1 + 1 = 2 % 1 WHERE 1 > (1 % 1) GROUP BY test1, 1 ORDER BY test2, 1 DESC LIMIT 1;"
-                     "DROP TABLE IF EXISTS test;"
-                     "DELETE FROM test WHERE 1 = 1 AND 1 > 2 + 3 * 5;"
-                     "UPDATE test SET a = 1 WHERE aaa < BBBB;"
-                     "INSERT INTO test (a, b, c) VALUES (\"val\"), (2);"
-                     "CREATE DATABASE IF NOT EXISTS db;"
-                     "CREATE TABLE tt("
-                     "    PersonID int AUTO_INCREMENT,\n"
-                     "    LastName varchar(255) DEFAULT NULL,\n"
-                     "    FirstName varchar(255) DEFAULT 'test',\n"
-                     "    Address varchar(255) NOT NULL,\n"
-                     "    City varchar(255) NOT NULL DEFAULT 'test' AUTO_INCREMENT,"
-                     "CHECK(PersonID > 2),"
-                     "UNIQUE(LastName, FirstName),"
-                     "PRIMARY KEY (PersonID, LastName, FirstName),"
-                     "FOREIGN KEY (PersonID, LastName, FirstName) REFERENCES tt@aa(id, tmp, name)"
-                     ");"
-                     "ALTER TABLE tmp ADD aaa INT,"
-                     "CHANGE aaa bbb,"
-                     "MODIFY bbb CHAR(5),"
-                     "DROP bbb;";
-    printf("---LEXER---\n");
-    Lexer lexicalScanner = Lexer();
-    vector<Symbol *> result = lexicalScanner.scan(request);
+    std::string request;
 
-    /*for (auto &i: result) {
-        i->display();
-    }*/
+    while (true) {
 
-    printf("---PARSER---\n");
+        printf(">> ");
+        std::getline(std::cin, request);
 
-    vector<Statement *> listStatement = Parser::parse(result);
+        if (request == "exit") {
+            break;
+        }
 
-    printf("size : %zu\n", listStatement.size());
-
-    for (auto &i: result) {
-        delete i;
+        Lexer lexicalScanner = Lexer();
+        vector<Symbol *> result = lexicalScanner.scan(request);
+        vector<Statement *> listStatement = Parser::parse(result);
     }
 
-    for (auto &i: listStatement) {
-        i->display();
-        delete i;
-    }
-
+    printf("Bye <3\n");
     return 0;
 }
